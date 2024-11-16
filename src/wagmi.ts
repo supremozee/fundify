9b79d032756d8d63897bee6cc5af0ba3f917ae9f
0xaplus <codewithaplus@gmail.com>
2024-11-16 23:22:01 +0100

    update chain data

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -2,22 +2,16 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { http } from "wagmi";
 import {
-  mainnet,
   taiko,
   taikoHekla,
 } from "wagmi/chains";
 
 export const config = getDefaultConfig({
-  appName: "RainbowKit demo",
+  appName: "Fundify",
   projectId: "3e5fb9eb794c48260e2a5a41be9e5b38",
-  chains: [
-    mainnet,
-    taiko,
-    taikoHekla,
-  ],
+  chains: [taiko, taikoHekla],
   transports: {
-    [mainnet.id]: http(
-      "https://eth-mainnet.g.alchemy.com/v2/8ywJ2qEHhB4TgLYz5ytdQiRyVHRNcoQT"
-    ),
+    [taiko.id]: http("https://rpc.mainnet.taiko.xyz"),
+    [taikoHekla.id]: http("https://rpc.hekla.taiko.xyz"),
   },
 });
